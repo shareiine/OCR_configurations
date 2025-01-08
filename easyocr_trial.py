eaf90efdcc35586580474262192df2acefa72dde
shareiine <artemisreigne@gmail.com>
2025-01-08 12:10:47 +0800

    Basic EasyOCR

---
+++ easyocr_trial.py
@@ -0,0 +1,27 @@
+# USAGE
+# python normal_OCR.py -i image1.jpg
+
+# import necessary packages
+import cv2
+import easyocr
+import argparse
+
+
+# construct the argument parser
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True, help = "Path to the Input Image")
+args = vars(ap.parse_args())
+
+# determine the languages to be read
+reader = easyocr.Reader(['en', 'tl'])
+
+# load image
+image = cv2.imread(args["image"])
+
+# convert to text
+txt = reader.readtext(image)
+
+# display text
+print(txt)
+
+# draw box and display text
